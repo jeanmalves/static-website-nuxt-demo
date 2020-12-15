310cba5d4f40c1642bde0075c71c96348ea6c3ba
Jeam Alves <jeanmalves@gmail.com>
2020-12-15 20:54:53 -0300

    images from website

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="site-header">
+    <div class="container">
+      <div class="site-header-inner">
+        <div class="brand header-brand">
+          <h1 class="m-0">
+            <a href="#">
+              <img
+                class="header-logo-image"
+                src="~/assets/images/logo.svg"
+                alt="Logo"
+              />
+            </a>
+          </h1>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
