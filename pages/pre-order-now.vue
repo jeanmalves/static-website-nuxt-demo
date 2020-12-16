27fd9019b4780256597f02ffef6bf896e6b118e2
Jeam Alves <jeanmalves@gmail.com>
2020-12-16 14:52:15 -0300

    Pre order page

---
+++ pages/pre-order-now.vue
@@ -0,0 +1,74 @@
+<template>
+  <main>
+    <section class="pricing section">
+      <div class="container-sm">
+        <div class="pricing-inner section-inner">
+          <div class="pricing-header text-center">
+            <h2 class="section-title mt-0">Unlimited for all</h2>
+            <p class="section-paragraph mb-0">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut ad
+              quis nostrud.
+            </p>
+          </div>
+          <div class="pricing-tables-wrap">
+            <div class="pricing-table">
+              <div class="pricing-table-inner">
+                <div class="pricing-table-main">
+                  <div class="pricing-table-header pb-24">
+                    <div class="pricing-table-price">
+                      <span class="pricing-table-price-currency h2">$</span
+                      ><span class="pricing-table-price-amount h1">49</span
+                      ><span class="text-xs">/month</span>
+                    </div>
+                  </div>
+                  <div class="pricing-table-features-title text-xs pt-24 pb-24">
+                    What you will get
+                  </div>
+                  <ul class="pricing-table-features list-reset text-xs">
+                    <li>
+                      <span>Lorem ipsum dolor sit nisi</span>
+                    </li>
+                    <li>
+                      <span>Lorem ipsum dolor sit nisi</span>
+                    </li>
+                    <li>
+                      <span>Lorem ipsum dolor sit nisi</span>
+                    </li>
+                    <li>
+                      <span>Lorem ipsum dolor sit nisi</span>
+                    </li>
+                  </ul>
+                </div>
+                <div class="pricing-table-cta mb-8">
+                    <NuxtLink class="button button-primary button-shadow button-block" to="/contact">
+                        Pre order now
+                    </NuxtLink>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="cta section">
+      <div class="container">
+        <div class="cta-inner section-inner">
+          <h3 class="section-title mt-0">Still not convinced on buying?</h3>
+          <div class="cta-cta">
+            <a class="button button-primary button-wide-mobile" href="/contact"
+              >Get in touch</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
